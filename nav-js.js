ac9d3833f841ff53e3b9f2bd16ce4266a0cf8f0f
Marina Zujic <zujicmarina@gmail.com>
2020-03-23 09:03:15 +0100

    selectors changed

--- nav-js.js
+++
@@ -1,8 +0,0 @@
-let mainNav = document.querySelector(".opened-list");
-
-let navBarToggle = document.querySelector(".menu-icon");
-
-navBarToggle.addEventListener("click", function () {
-    
-    mainNav.classList.toggle("menu-list");
-});
