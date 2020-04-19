777e7421f97ae89505978e61bbc8d5759885ff80
Marina Zujic <zujicmarina@gmail.com>
2020-04-19 20:33:11 +0200

    Index page done

--- JS/js-app.js
+++ JS/js-app.js
@@ -1,8 +1,45 @@
-let mainNav = document.getElementById("menu");
-
-let navBarToggle = document.querySelector(".menu__toggle");
-
-navBarToggle.addEventListener("click", function () {
-
-    mainNav.classList.toggle("menu-opened");
-});
+document.addEventListener('DOMContentLoaded', function () {
+    //BUTTON
+    let mainNav = document.querySelector(".menu");
+  
+    let navBarToggle = document.querySelector(".nav__button");
+  
+    navBarToggle.addEventListener("click", function () {
+      mainNav.classList.toggle("menu-opened");
+    });
+  
+    //GLIDE PLUGIN
+    const mainGlide = new Glide("section.slider > div", {
+      type: 'carousel',
+      autoplay: true,
+      perView: 1,
+      autoplay: 3000,
+      animationDuration: 2000
+    });
+    mainGlide.mount();
+  
+    let reviewSlider = new Glide("section.section-reviews > div", {
+      type: 'carousel',
+      animationDuration: 1000,
+      gap: 0
+    });
+    reviewSlider.mount();
+  
+    let awardsSlider = new Glide("section.section-awards > div", {
+      type: 'carousel',
+      autoplay: true,
+      perView: 5,
+      autoplay: 1000,
+      animationDuration: 2000,
+      breakpoints: {
+        768: {
+          perView: 4
+        },
+        500: {
+          perView: 3
+        },
+      },
+      gap: 0
+    });
+    awardsSlider.mount();
+  });
