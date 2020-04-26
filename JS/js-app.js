cf6a4b03b18be000952424f666b64e721d5df94e
Marina Zujic <zujicmarina@gmail.com>
2020-04-26 12:55:18 +0200

    Page contact sunday update

--- JS/js-app.js
+++ JS/js-app.js
@@ -10,67 +10,75 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   //GLIDE PLUGIN
-  const mainGlide = new Glide("section.slider > div", {
-    type: 'carousel',
-    autoplay: true,
-    perView: 1,
-    autoplay: 3000,
-    animationDuration: 2000
-  });
-  mainGlide.mount();
+  if (document.querySelector('#header-slider')) {
+    const mainGlide = new Glide("#header-slider", {
+      type: 'carousel',
+      autoplay: true,
+      perView: 1,
+      autoplay: 3000,
+      animationDuration: 2000
+    });
+    mainGlide.mount();
+  }
 
-  let reviewSlider = new Glide("section.section-reviews > div", {
-    type: 'carousel',
-    animationDuration: 1000,
-    gap: 0
-  });
-  reviewSlider.mount();
+  if (document.querySelector('#header-slider')) {
+    const reviewSlider = new Glide("#review-slider", {
+      type: 'carousel',
+      animationDuration: 1000,
+      gap: 0
+    });
+    reviewSlider.mount();
+  }
 
-  let awardsSlider = new Glide("section.section-awards > div", {
-    type: 'carousel',
-    autoplay: true,
-    perView: 5,
-    autoplay: 1000,
-    animationDuration: 2000,
-    breakpoints: {
-      768: {
-        perView: 4
+  if (document.querySelector('#awards-slider')) {
+    const awardsSlider = new Glide("#awards-slider", {
+      type: 'carousel',
+      autoplay: true,
+      perView: 5,
+      autoplay: 1000,
+      animationDuration: 2000,
+      breakpoints: {
+        768: {
+          perView: 4
+        },
+        500: {
+          perView: 3
+        },
       },
-      500: {
-        perView: 3
-      },
-    },
-    gap: 0
-  });
-  awardsSlider.mount();
+      gap: 0
+    });
+    awardsSlider.mount();
+  }
 
-  //slider on about page
-  let aboutSlider = new Glide("section.section-awards.section__container > div", {
-    type: 'carousel',
-    autoplay: true,
-    perView: 5,
-    autoplay: 1000,
-    animationDuration: 2000,
-    breakpoints: {
-      768: {
-        perView: 4
+  if (document.querySelector('#about-awards-slider')) {
+    //slider on about page
+    let aboutSlider = new Glide("#about-awards-slider", {
+      type: 'carousel',
+      autoplay: true,
+      perView: 5,
+      autoplay: 1000,
+      animationDuration: 2000,
+      breakpoints: {
+        768: {
+          perView: 4
+        },
+        500: {
+          perView: 3
+        },
       },
-      500: {
-        perView: 3
-      },
-    },
-    gap: 0
-  });
-  aboutSlider.mount();
+      gap: 0
+    });
+    aboutSlider.mount();
+  }
 
   // Initialize and add the map
-   function initMap() {
-  // The location of Oslo
-    var oslo = {lat: 59.91273, lng: 10.74609};
-  // The map, centered at Oslo
-   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: oslo});
-  // The marker, positioned at Oslo
-   var marker = new google.maps.Marker({position: oslo, map: map});
-}
+  function initMap() {
+    // The location of Oslo
+    var oslo = { lat: 59.91273, lng: 10.74609 };
+    // The map, centered at Oslo
+    var map = new google.maps.Map(
+      document.getElementById('map'), { zoom: 4, center: oslo });
+    // The marker, positioned at Oslo
+    var marker = new google.maps.Marker({ position: oslo, map: map });
+  }
 });
