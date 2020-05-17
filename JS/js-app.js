09b324c09c90589d6b341b692a9b7037766b55ab
Milos Radibratovic <radibratovic.milos@gmail.com>
2020-05-17 12:15:35 +0200

    Modified Portfolio page and form error handling

--- JS/js-app.js
+++ JS/js-app.js
@@ -70,70 +70,62 @@ document.addEventListener('DOMContentLoaded', function () {
     aboutSlider.mount();
   }
 
- //FORM VALIDATION
+  //FORM VALIDATION
+  if (document.querySelector('#contact-form')) {
+    const form = document.getElementById('contact-form');
+    const feedback = document.querySelector('.feedback');
 
-  const form = document.getElementById('contact-form');
-  const feedback = document.querySelector('.feedback');
-
-  //regexp for validation
-  const namePattern = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
-  const emailValid = /^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(domain|domain2)\.com$/;
-  const phoneValid = /^(0047|\+47|47)?[2-9]\d{7}$/;
-  const cityValid = /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/;
-  const textLenght = /^\s*(?:\S\s*){10,100}$/;
-
-  form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    //name validation
-    const name = form.name.value;
-
-    if (namePattern.test(name)) {
-      // feedback good info
-      return true;
-    } else {
-      //fedback help
-      feedback.textContent = 'Name must contain letters only!';
-    }
-    
-    //email validation
-    const email = form.email.value;
-
-    if (emailValid.test(email)) {
-      return true;
-    } else {
-      feedback.textContent = 'You have entered an invalid email address!';
-    }
-
-    //phone validation
-    const phone = form.phone.value;
-
-    if (phoneValid.test(phone)) {
-      return true;
-    } else {
-      feedback.textContent = 'You have entered an invalid phone number!';
-    }
-
-    //city validation
-    const city = form.city.value;
-
-    if (cityValid.test(city)) {
-      return true;
-    } else {
-      feedback.textContent = 'You have entered an invalid city name!';
-    }
-
-    //text area max 100 characters
-    const text = form.text.value;
-
-    if (textLenght.test(text)) {
-      return true;
-    } else {
-      feedback.textContent = 'Your text must be 100 characters or less!';
-    }
-       
-  });
+    //regexp for validation
+    const namePattern = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+    const emailValid = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    const phoneValid = /^(0047|\+47|47)?[2-9]\d{7}$/;
+    const cityValid = /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/;
+    // const textLenght = /^\s*(?:\S\s*){10,100}$/;
 
 
-  
+
+    form.querySelector('input[type="submit"]').addEventListener('click', function (e) {
+      e.preventDefault();
+
+      //name validation
+      const name = form.name.value;
+      if (!namePattern.test(name)) {
+        form.querySelector('[name="name"] + .feedback').classList.add('show');
+      } else {
+        form.querySelector('[name="name"] + .feedback').classList.remove('show');
+      }
+
+      //email validation
+      const email = form.email.value;
+      if (!emailValid.test(email)) {
+        form.querySelector('[name="email"] + .feedback').classList.add('show');
+      } else {
+        form.querySelector('[name="email"] + .feedback').classList.remove('show');
+      }
+
+      //phone validation
+      const phone = form.phone.value;
+      if (!phone) {
+        form.querySelector('[name="phone"] + .feedback').classList.add('show');
+      } else {
+        form.querySelector('[name="phone"] + .feedback').classList.remove('show');
+      }
+
+      //city validation
+      const city = form.city.value;
+      if (!cityValid.test(city)) {
+        form.querySelector('[name="city"] + .feedback').classList.add('show');
+      } else {
+        form.querySelector('[name="city"] + .feedback').classList.remove('show');
+      }
+
+      //text area max 100 characters
+      const text = form.text.value;
+      if (!(text.length < 100)) {
+        form.querySelector('[name="text"] + .feedback').classList.add('show');
+      } else {
+        form.querySelector('[name="text"] + .feedback').classList.remove('show');
+      }
+    });
+  }
 });
